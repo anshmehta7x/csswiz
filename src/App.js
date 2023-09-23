7b73751295b954e679c2da92a973fb930a1e59a4
ansh <anshkapilmehta@gmail.com>
2023-09-24 01:57:10 +0530

    sorted files in directories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
+import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
 
